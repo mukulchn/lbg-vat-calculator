37d1bc6472d4ec85cb95860af1a0b6e40ed998d9
Mukul <mukul_chn@hotmail.com>
2025-02-26 15:22:10 +0000

    added commented code to see SQ quality gate fail

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// exlain what this does
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
 test('renders VAT Calculator text', () => {
   render(<App />);
   const heading = screen.getByText(/VAT CALCULATOR/i);
