7cd8d8e91ebb01a8a1b4a4f73cd24b0b9a189527
Mukul <mukul_chn@hotmail.com>
2025-02-26 15:23:45 +0000

    redo

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
+// exlain what this does
 test('renders VAT Calculator text', () => {
   render(<App />);
   const heading = screen.getByText(/VAT CALCULATOR/i);
